20545503ad054cd792523963ba4abc09421365d1
Stefano <stefano.zanoni00@gmail.com>
2023-06-15 17:33:04 +0200

    built a temp model

--- src/model.py
+++ src/model.py
@@ -1,19 +1,14 @@
 from transformers import TFAutoModelForTokenClassification
 from transformers import create_optimizer
 
-import keras
+import tensorflow as tf
+
+def model_ner(model_name, len_labels, id_label, label_id):
+    return TFAutoModelForTokenClassification.from_pretrained(model_name, num_labels=len_labels,
+                                                             id2label=id_label, label2id=label_id)
 
 
-def model_ner(model_name, labels):
-    id_label = {i: label for i, label in enumerate(labels)}
-    label_id = {label: i for i, label in enumerate(labels)}
-
-    model = TFAutoModelForTokenClassification.from_pretrained(model_name, num_labels=len(labels),
-                                                              id2label=id_label, label2id=label_id)
-    return model
-
-
-def get_optimizer(num_epochs, train_steps):
+def get_optimizer():
     optimizer, _ = create_optimizer(
         # https://datascience.stackexchange.com/questions/64583/what-are-the-good-parameter-ranges-for-bert-hyperparameters-while-finetuning-it
         # num_train_steps=train_steps, #training step is one gradient update
@@ -26,6 +21,16 @@ def get_optimizer(num_epochs, train_steps):
     # config the model with metrics and losses
 
 
-def create_model(model_ner, optimizer):
-    # model.compile(optimizer=optimizer)
-    return
+def create_model(bert_model, len_labels, id_label, label_id, max_input_len):
+    ner_model = model_ner(bert_model, len_labels, id_label, label_id)
+    input_ids = tf.keras.Input(shape=(max_input_len,), dtype=tf.int32)
+    input_mask = tf.keras.Input(shape=(max_input_len,), dtype=tf.int32)
+    input_labels = tf.keras.Input(shape=(max_input_len,), dtype=tf.int32)
+    ner_output = ner_model(input_ids, attention_mask=input_mask, labels=input_labels, return_dict=True)
+    x = tf.keras.layers.Dense(10, activation='relu')(ner_output['last_hidden_state'])
+    output = tf.keras.layers.Dense(2, activation='sigmoid')(x)
+    model = tf.keras.models.Model(inputs=[input_ids, input_mask, input_labels], outputs=[output])
+    optimizer = get_optimizer()
+    model.compile(optimizer=optimizer)
+
+    return model
--- src/training.py
+++ src/training.py
@@ -1,13 +1,14 @@
 from src.data_utilities import k_fold
 
-import keras_tuner
+import keras
+
+n_splits = 10
 
 
-def training(data_x, data_y, n_splits, model, callbacks):
-    for train_index, val_index in k_fold(data_x, n_splits):
+def training(data_x, data_y, model):
+    for train_index, val_index in k_fold(data_x, data_y, n_splits):
         train_x, train_y, val_x, val_y = data_x[train_index], data_y[train_index], val_x[val_index], val_y[val_index]
-        model.grid_search(train_x, train_y,
-                          epochs=10,
-                          batch_size=2,
-                          validation_data=(val_x, val_y),
-                          callbacks=callbacks)
+        model.fit(train_x, train_y,
+                  epochs=10,
+                  batch_size=2,
+                  validation_data=(val_x, val_y))
