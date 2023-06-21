88a44afa6676da9ffc4f21c4f664882a2b4bdccc
Stefano <stefano.zanoni00@gmail.com>
2023-06-21 13:25:54 +0200

    added multi-gpu training

--- src/main.py
+++ src/main.py
@@ -1,25 +1,54 @@
-from src.data_utilities import load_data, split_train_test, tokenize_text, \
-    compute_iob, pre_process_texts, get_labels_id, get_bert_inputs, get_bert_outputs
-from src.model import create_model
-from src.training import training
+import os
+
+import torch
+import torch.multiprocessing as mp
+from torch.distributed import init_process_group, destroy_process_group
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 from transformers import BertTokenizerFast
+
+from src.data_utilities import load_data, split_train_test, tokenize_text, \
+    compute_iob, pre_process_texts, get_labels_id, get_bert_inputs, get_re_outputs, count_drug_effects
+from src.model_ner import NerModel
+from src.training import TrainerNer
 
 bert_model = 'emilyalsentzer/Bio_ClinicalBERT'
 tokenizer = BertTokenizerFast.from_pretrained(bert_model)
 
 
-def main():
+def main(rank, world_size, save_every, total_epochs=10, batch_size=32):
+
+    ddp_setup(rank, world_size)
+
     data = load_data()
     pre_process_texts(data)
     compute_iob(data)
     id_label, label_id, len_labels = get_labels_id(data)
-    tokenized_texts, tokenized_labels = tokenize_text(data, tokenizer)
-    inputs = get_bert_inputs(tokenized_texts, tokenized_labels, tokenizer, label_id)
-    outputs, num_drugs, num_effects = get_bert_outputs(data)
-    train_in, test_in, train_out, test_out = split_train_test(inputs, outputs)
-    model = create_model(bert_model, len_labels, id_label, label_id, num_drugs, num_effects)
-    training(train_in, train_out, model)
+    num_drugs, num_effects = count_drug_effects(data)
+
+    train_in, test_in, train_out_ner, train_out_re, test_out_ner, test_out_re = split_train_test(data)
+
+    tokenized_texts_train, tokenized_labels_train = tokenize_text(train_in, train_out_ner, tokenizer)
+    tokenized_texts_test, tokenized_labels_test = tokenize_text(test_in, test_out_ner, tokenizer)
+    inputs_train = get_bert_inputs(tokenized_texts_train, tokenized_labels_train, tokenizer, label_id)
+    # prepare the data to multi-gpu training
+    inputs_train = DataLoader(inputs_train, batch_size=batch_size, shuffle=False, sampler=DistributedSampler(inputs_train))
+
+    inputs_test = get_bert_inputs(tokenized_texts_test, tokenized_labels_test, tokenizer, label_id)
+    train_out_re = get_re_outputs(train_out_re)
+    test_out_re = get_re_outputs(test_out_re)
+    ner = NerModel(bert_model, len_labels, id_label, label_id)
+    optimizer = ner.get_optimizer()
+    ner_training_steps = total_epochs * len(inputs_train) / batch_size
+    scheduler = ner.get_scheduler(ner_training_steps)
+    ner_trainer = TrainerNer(ner, inputs_train, optimizer, scheduler, rank, save_every)
+    ner_trainer.train_ner(total_epochs)
+    destroy_process_group()
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+
+    save_every = int(sys.argv[1])
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, save_every), nprocs=world_size)
--- src/training.py
+++ src/training.py
@@ -1,13 +1,80 @@
-n_splits = 1
+import torch
+
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP
+from tqdm import tqdm
 
 
-def training(data_x, data_y, model):
-    # for train_index, val_index in k_fold(data_x, n_splits):
-    #     train_x = [data_x[i] for i in train_index]
-    #     train_y = [data_y[i] for i in train_index]
-    #     val_x = [data_x[i] for i in val_index]
-    #     val_y = [data_y[i] for i in val_index]
-    history = model.fit([data_x[0], data_x[1]], [data_x[2], (data_y[0], data_y[1])],
-                        epochs=1,
-                        batch_size=1)
-    print(history.history['train_loss'])
+def training_ner(train_d_loader, model, optimizer, device, scheduler):
+    model.train()
+    loss_results_train = 0
+    for data in tqdm(train_d_loader):
+        for k, v in data.items():
+            data[k] = v.to(device)
+        # backward propagation
+        optimizer.zero_grad()
+        _, loss = model(**data)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        loss_results_train += loss.item()
+    return model, loss_results_train / len(train_d_loader)
+
+
+def validation_ner(val_d_loader, model, optimizer, device, scheduler):
+    model.eval()  # for validation - not .train()
+    loss_results_val = 0
+    for data in tqdm(val_d_loader):
+        for k, v in data.items():
+            data[k] = v.to(device)
+        _, loss = model(**data)
+        loss_results_val += loss.item()
+    return loss_results_val / len(val_d_loader)
+
+
+class TrainerNer:
+    def __init__(self,
+                 model: torch.nn.Module,
+                 train_data: DataLoader,
+                 optimizer: torch.optim.Optimizer,
+                 scheduler: torch.optim.lr_scheduler.LambdaLR,
+                 gpu_id: int,
+                 save_every: int
+                 ) -> None:
+        self.gpu_id = gpu_id
+        self.model = DDP(model, device_ids=[gpu_id])
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.save_evey = save_every
+
+    def _run_batch_ner(self, source):
+        self.optimizer.zero_grad()
+        loss = self.model(source)
+        loss.backward()
+        self.optimizer.step()
+        self.scheduler.step()
+        return loss
+
+    def _run_epoch_ner(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batch-size: {b_sz} | Steps {len(self.train_data)}")
+        loss_batch = 0
+        for source in self.train_data:
+            source = source.to(self.gpu_id)
+            loss_batch += self._run_batch_ner(source)
+        return loss_batch / len(self.train_data)
+
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.module.state_dict()
+        torch.save(ckp, "checkpoint.pt")
+        print(f"Epoch {epoch} | Training checkpoint saved at checkpoint.pt")
+
+    def train_ner(self, max_epochs: int):
+        epoch_loss_means = []
+        for epoch in range(max_epochs):
+            epoch_loss_mean = self._run_epoch_ner(epoch)
+            epoch_loss_means.append(epoch_loss_mean)
+            if self.gpu_id == 0 and epoch % self.save_evey == 0:
+                self._save_checkpoint(epoch)
+        return epoch_loss_means
