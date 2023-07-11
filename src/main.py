09a4d2f04499206923447bdcfcbba46a70417e1c
Stefano <stefano.zanoni00@gmail.com>
2023-07-11 15:12:48 +0200

    minor changes

--- src/main.py
+++ src/main.py
@@ -162,11 +162,11 @@ def main(rank, world_size, save_every=10, epochs=10, batch_size=32, ner_input_le
     else:
         data_re = pd.read_csv("../data/re.csv", converters={'annotated_text': literal_eval, 'pos_tags': literal_eval})
 
-    # ner_model, final_inputs, id_label = \
-    #     train_ner(data_ner, epochs, batch_size, rank, save_every, world_size, ner_input_length)
+    ner_model, final_inputs, id_label = \
+        train_ner(data_ner, epochs, batch_size, rank, save_every, world_size, ner_input_length)
 
-    re_model, final_outputs = \
-        train_re(data_re, epochs, batch_size, rank, save_every, world_size, re_input_length)
+    # re_model, final_outputs = \
+    #     train_re(data_re, epochs, batch_size, rank, save_every, world_size, re_input_length)
 
     # final_model = FinalModel(ner_model, re_model, tokenizer, id_label, rank, re_input_length)
     # test_final(final_model, final_inputs, final_outputs, batch_size, world_size, rank)
